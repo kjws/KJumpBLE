3466eda5661824f81a84a7110178889acb22b48d
rulerhao <z19970615@gmail.com>
2021-12-13 10:12:09 +0800

    1. Move uuid list to one file, that it may be more readable.
    From Jake
    2021/12/13 10:12(GMT+8)

---
+++ app/src/main/java/com/example/kjumpble/ble/uuid/KI8360UUIDList.java
@@ -0,0 +1,11 @@
+package com.example.kjumpble.ble.uuid;
+
+import java.util.UUID;
+
+public class KI8360UUIDList {
+    public static UUID KJUMP_CHARACTERISTIC_CONFIG_UUID = UUID.fromString("0000fff0-0000-1000-8000-00805f9b34fb");
+
+    public static UUID KJUMP_CHARACTERISTIC_DESCRIBE_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+    public static UUID KJUMP_CHARACTERISTIC_READ_UUID = UUID.fromString("0000fff1-0000-1000-8000-00805f9b34fb");
+    public static UUID KJUMP_CHARACTERISTIC_WRITE_UUID = UUID.fromString("0000fff3-0000-1000-8000-00805f9b34fb");
+}
