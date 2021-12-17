12d34937a1a19f76065cf1250c1168a0daaf82ff
rulerhao <z19970615@gmail.com>
2021-12-17 09:19:52 +0800

    1. Rename UUID List.
    From Jake
    2021/12/17 09:19(GMT+8)

--- app/src/main/java/com/example/kjumpble/ble/uuid/KjumpUUIDList.java
+++ app/src/main/java/com/example/kjumpble/ble/uuid/KjumpUUIDList.java
@@ -2,7 +2,7 @@ package com.example.kjumpble.ble.uuid;
 
 import java.util.UUID;
 
-public class KI8360UUIDList {
+public class KjumpUUIDList {
     public static UUID KJUMP_CHARACTERISTIC_CONFIG_UUID = UUID.fromString("0000fff0-0000-1000-8000-00805f9b34fb");
 
     public static UUID KJUMP_CHARACTERISTIC_DESCRIBE_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
