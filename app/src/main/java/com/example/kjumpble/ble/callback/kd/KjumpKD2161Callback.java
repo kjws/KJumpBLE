a6274ade9df1a93fe5b2f652aaa1c445c725c632
rulerhao <z19970615@gmail.com>
2021-12-20 13:40:11 +0800

    1. Add KD-2161 callback
    From Jake
    2021/12/20 13:40(GMT+8)

---
+++ app/src/main/java/com/example/kjumpble/ble/callback/kd/KjumpKD2161Callback.java
@@ -0,0 +1,33 @@
+package com.example.kjumpble.ble.callback.kd;
+
+import com.example.kjumpble.ble.data.DataFormatOfKI8360;
+
+public class KjumpKD2161Callback {
+    public void onSetDeviceFinished (boolean success) {
+
+    }
+
+    public void onGetNumberOfData (int number) {
+
+    }
+
+    public void onGetIndexMemory (int index, DataFormatOfKI8360 data) {
+
+    }
+
+    public void onClearAllDataFinished (boolean success) {
+
+    }
+
+    public void onWriteUnitFinished (boolean success) {
+
+    }
+
+    public void onWriteClockFinished (boolean success) {
+
+    }
+
+    public void onWriteReminderClockFinished (boolean success) {
+
+    }
+}
