eba287faa142a898e55129ea33156db2c94af183
rulerhao <z19970615@gmail.com>
2021-12-22 15:06:11 +0800

    1. Delete unused command.
    From Jake
    2021/12/22 15:06(GMT+8)

--- app/src/main/java/com/example/kjumpble/ble/cmd/util/KD2070Util.java
+++ app/src/main/java/com/example/kjumpble/ble/cmd/util/KD2070Util.java
@@ -4,10 +4,6 @@ import com.example.kjumpble.ble.format.LeftRightHand;
 import com.example.kjumpble.ble.format.TemperatureUnit;
 
 public class KD2070Util {
-    public static final byte[] writeUnitCmd = new byte[]{0x03, 0x01, 0x00, 0x6b,
-            0x04}; // 分秒
-    public static final byte[] writeHandCmd = new byte[] {0x03, 0x01, 0x00, 0x6b
-            , 0x04};
     // Temperature unit
     public static byte getUnitData (byte data, TemperatureUnit unit) {
         if ((data & 0x01) == 0x01) {
