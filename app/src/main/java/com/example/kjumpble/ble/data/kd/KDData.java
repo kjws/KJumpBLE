2cb86d62b1b68e281b99eab31f49af1e297fd305
rulerhao <z19970615@gmail.com>
2021-12-24 09:28:17 +0800

    1. Rename DataFormatOfKD to KDData
    From Jake.

---
+++ app/src/main/java/com/example/kjumpble/ble/data/kd/KDData.java
@@ -0,0 +1,42 @@
+package com.example.kjumpble.ble.data.kd;
+
+import android.util.Log;
+
+import com.example.kjumpble.ble.SensePositionEnum;
+import com.example.kjumpble.ble.timeFormat.ClockTimeFormat;
+
+public class KDData {
+    ClockTimeFormat time;
+    float Temperature;
+
+    public KDData (byte[] data) {
+        int Year = 2000 + data[1];
+        int Month = data[2];
+        int Day = data[3];
+        int Hour = data[4];
+        int Minute = data[5];
+        int Second = data[6];
+        time = new ClockTimeFormat(Year, Month, Day, Hour, Minute, Second);
+        Temperature = (float) (((data[7] & 0xff) + (data[8] & 0xff) * 256) / 100.0);
+
+//        logAll();
+    }
+
+    public ClockTimeFormat getTime () {
+        return time;
+    }
+
+    public float getTemperature () {
+        return Temperature;
+    }
+
+    private void logAll() {
+        Log.d("test8360", "year = " + time.getYear());
+        Log.d("test8360", "Month = " + time.getMonth());
+        Log.d("test8360", "Day = " + time.getDay());
+        Log.d("test8360", "Hour = " + time.getHour());
+        Log.d("test8360", "Minute = " + time.getMinute());
+        Log.d("test8360", "Second = " + time.getSecond());
+        Log.d("test8360", "Temperature = " + Temperature);
+    }
+}
